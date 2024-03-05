7a74c8307104eeb17738c2d36eeed7a41934267d
Katada <katada.alrawi@dci.education>
2024-03-05 19:33:37 +0100

    update for the day 1

---
+++ doctor-appointent/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1> Hallo Katada</h1>
+    <Button>Push Me </Button>
+   </div>
+  );
+}
